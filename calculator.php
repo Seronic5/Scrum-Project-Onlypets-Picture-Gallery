6ea3e3fe65cc733e97855054625ead3913883450
Seronic5 <st.armgart@web.de>
2024-11-25 16:35:17 +0100

    first day progress

---
+++ calculator.php
@@ -0,0 +1,143 @@
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Calculator</title>
+</head>
+<body>
+<div class="outer_box"> <!-- rand -->
+    <div class="top_line">  <!-- top line -->
+        <div class="calc_img"><img src="img/calc.png" alt="Calculator" width="15" height="8"> <!-- img rechner -->
+
+        </div class="calculator_text">
+        <div> <!-- calculator text -->
+
+        </div>
+        <div> <!-- minimize img -->
+
+        </div>
+        <div> <!-- maximize img -->
+
+        </div>
+        <div> <!-- close x -->
+
+        </div>
+    </div>
+    <div> <!-- second line -->
+        <div> <!-- burger -->
+
+        </div>
+        <div> <!-- Standard -->
+
+        </div>
+        <div> <!-- change to smaller version -->
+
+        </div>
+        <div> <!-- clock history-->
+        </div>
+
+    </div>
+    <div> <!-- ausgabe  third line -->
+
+    </div>
+    <div> <!-- 4th line -->
+        <div> <!-- Memory Clear set to 0 -->
+
+        </div>
+        <div> <!-- Memory Recall the number in Memory to display -->
+
+        </div>
+        <div> <!-- The M+ key add the number in the display to or from the current number in the memory. -->
+
+        </div>
+        <div> <!-- The M- key subtract the number in the display to or from the current number in the memory. -->
+
+        </div>
+        <div> <!-- MS(Memory save) key will Store the current display in the memory, replacing what was already there -->
+
+        </div>
+        <div> <!-- memory popupp shows all saved memorys and puts them on screen on click replacing current number in -->
+
+        </div>
+
+    </div>
+    <div> <!-- 5th line keys -->
+        <div> <!-- % prozent calculation -->
+
+        </div>
+        <div> <!-- CE cancel entry set inputfield set it to 0 -->
+
+        </div>
+        <div> <!-- C clear current calculation also set inputfield it to 0 -->
+
+        </div>
+        <div> <!-- x rewind input  from a stack? -->
+
+        </div>
+        <div> <!-- reciprocal (1/x)  The reciprocal of a number can be determined by dividing the variable by 1. Similarly, the reciprocal of a function is determined by dividing 1 by the function's expression. Example: Given a function f(y) , its reciprocal function is 1/f(y). -->
+
+        </div>
+        <div> <!-- x² multiply the number with itself -->
+
+        </div>
+        <div> <!-- square root -->
+
+        </div>
+
+        <div> <!-- divide -->
+
+        </div>
+        <div> <!-- 7 -->
+
+        </div>
+        <div> <!-- 8 -->
+
+        </div>
+
+        <div> <!-- 9 -->
+
+        </div>
+        <div> <!-- multiply -->
+
+        </div>
+        <div> <!-- 4 -->
+
+        </div>
+        <div> <!-- 5 -->
+
+        </div>
+        <div> <!-- 6 -->
+
+        </div>
+        <div> <!-- subtract -->
+
+        </div>
+        <div> <!-- 1 -->
+
+        </div>
+        <div> <!-- 2 -->
+
+        </div>
+        <div> <!-- 3 -->
+
+        </div>
+        <div> <!-- addition -->
+
+        </div>
+        <div> <!-- negate -->
+
+        </div>
+        <div> <!-- 0 -->
+
+        </div> <!-- . -->
+        <div> <!-- = -->
+
+        </div>
+    </div>
+</div>
+<?php
+
+?>
+</body>
+</html>
