b1e3ca756efbdf8dd40250a38a5c4164ecec67ea
Seronic5 <st.armgart@web.de>
2024-11-26 16:28:17 +0100

    second day progress

--- calculator.php
+++ calculator.php
@@ -8,130 +8,141 @@
 <body>
 <div class="outer_box"> <!-- rand -->
     <div class="top_line">  <!-- top line -->
-        <div class="calc_img"><img src="img/calc.png" alt="Calculator" width="15" height="8"> <!-- img rechner -->
-
-        </div class="calculator_text">
-        <div> <!-- calculator text -->
+        <div class="calc_img"><img src="img/calc.png" alt="Calculator" width="35" height="25"> <!-- img rechner -->
 
         </div>
-        <div> <!-- minimize img -->
+        <div class="calculator_text"> Yoimiya<!-- calculator text -->
 
         </div>
-        <div> <!-- maximize img -->
+        <div><img src="img/minimize.png" alt="min" width="40" height="25"> <!-- minimize img -->
 
         </div>
-        <div> <!-- close x -->
+        <div><img src="img/maximize.png" alt="max" width="45" height="25"> <!-- maximize img -->
+
+        </div>
+        <div><img src="img/close.png" alt="close" width="45" height="25"> <!-- close x -->
 
         </div>
     </div>
-    <div> <!-- second line -->
-        <div> <!-- burger -->
+    <div class="second_line"> <!-- second line -->
+        <div class="second_img"><img src="img/burger.png" alt="burger" width="40" height="35"> <!-- burger -->
 
         </div>
-        <div> <!-- Standard -->
+        <div class ="calculator_text2"> Calculator <!-- Standard -->
 
         </div>
-        <div> <!-- change to smaller version -->
+        <div class="second_img"><img src="img/swap.png" alt="swap" width="40" height="35"> <!-- change to smaller version -->
 
         </div>
-        <div> <!-- clock history-->
-        </div>
-
-    </div>
-    <div> <!-- ausgabe  third line -->
-
-    </div>
-    <div> <!-- 4th line -->
-        <div> <!-- Memory Clear set to 0 -->
+        <div>
 
         </div>
-        <div> <!-- Memory Recall the number in Memory to display -->
-
-        </div>
-        <div> <!-- The M+ key add the number in the display to or from the current number in the memory. -->
-
-        </div>
-        <div> <!-- The M- key subtract the number in the display to or from the current number in the memory. -->
-
-        </div>
-        <div> <!-- MS(Memory save) key will Store the current display in the memory, replacing what was already there -->
-
-        </div>
-        <div> <!-- memory popupp shows all saved memorys and puts them on screen on click replacing current number in -->
-
+        <div class="second_img"><img src="img/history.png" alt="History" width="45" height="35"> <!-- clock history-->
         </div>
 
     </div>
-    <div> <!-- 5th line keys -->
-        <div> <!-- % prozent calculation -->
+    <div class="third_line"> <!-- 3rd line calculation over input field -->
+
+    </div>
+    <div class="fourth_line"><input Value="0" type="text"><!-- ausgabe  4th line -->
+
+    </div>
+    <div class="fifth_line"> <!-- 5th line -->
+    </div>
+    <div class="sixth_line"> <!-- 6th -->
+        <div class ="sixth_button"><button type="button" id="button_M" > MC </button> <!-- Memory Clear set to 0 -->
 
         </div>
-        <div> <!-- CE cancel entry set inputfield set it to 0 -->
+        <div class ="sixth_button"><button type="button" id="button_M" > MR </button><!-- Memory Recall the number in Memory to display -->
 
         </div>
-        <div> <!-- C clear current calculation also set inputfield it to 0 -->
+        <div class ="sixth_button"><button type="button" id="button_M" > M+ </button><!-- The M+ key add the number in the display to or from the current number in the memory. -->
 
         </div>
-        <div> <!-- x rewind input  from a stack? -->
+        <div class ="sixth_button"><button type="button" id="button_M" > M- </button><!-- The M- key subtract the number in the display to or from the current number in the memory. -->
 
         </div>
-        <div> <!-- reciprocal (1/x)  The reciprocal of a number can be determined by dividing the variable by 1. Similarly, the reciprocal of a function is determined by dividing 1 by the function's expression. Example: Given a function f(y) , its reciprocal function is 1/f(y). -->
+        <div class ="sixth_button"><button type="button" id="button_M" > MS </button><!-- MS(Memory save) key will Store the current display in the memory, replacing what was already there -->
 
         </div>
-        <div> <!-- x² multiply the number with itself -->
-
-        </div>
-        <div> <!-- square root -->
-
-        </div>
-
-        <div> <!-- divide -->
-
-        </div>
-        <div> <!-- 7 -->
-
-        </div>
-        <div> <!-- 8 -->
+        <div class ="sixth_button"><button type="button" id="button_M" > M </button><!-- memory popupp shows all saved memorys and puts them on screen on click replacing current number in -->
 
         </div>
 
-        <div> <!-- 9 -->
+    </div>
+    <div class="seventh_line"> <!-- 7th line keys -->
+        <div class ="seventh_button"><button type="button" id="button_field" > % </button> <!-- % prozent calculation -->
 
         </div>
-        <div> <!-- multiply -->
+        <div class ="seventh_button"> <button type="button" id="button_field"> CE </button> <!-- CE cancel entry set inputfield set it to 0 -->
 
         </div>
-        <div> <!-- 4 -->
+        <div class ="seventh_button"> <button type="button" id="button_field"> C </button> <!-- C clear current calculation also set inputfield it to 0 -->
 
         </div>
-        <div> <!-- 5 -->
+        <div class ="seventh_button"> <button type="button" id="button_field"> <= </button> <!-- x rewind input  from a stack? -->
 
         </div>
-        <div> <!-- 6 -->
+        <div class ="seventh_button"> <button type="button" id="button_field"> 1/x </button> <!-- reciprocal (1/x)  The reciprocal of a number can be determined by dividing the variable by 1. Similarly, the reciprocal of a function is determined by dividing 1 by the function's expression. Example: Given a function f(y) , its reciprocal function is 1/f(y). -->
 
         </div>
-        <div> <!-- subtract -->
+        <div class ="seventh_button"> <button type="button" id="button_field"> x² </button> <!-- x² multiply the number with itself -->
 
         </div>
-        <div> <!-- 1 -->
+        <div class ="seventh_button"> <button type="button" id="button_field"> ²√x </button> <!-- square root -->
 
         </div>
-        <div> <!-- 2 -->
+
+        <div class ="seventh_button"> <button type="button" id="button_field"> ÷ </button> <!-- divide -->
 
         </div>
-        <div> <!-- 3 -->
+        <div class ="seventh_button"> <button type="button" id="button_field"> 7 </button><!-- 7 -->
 
         </div>
-        <div> <!-- addition -->
+        <div class ="seventh_button"> <button type="button" id="button_field"> 8 </button> <!-- 8 -->
 
         </div>
-        <div> <!-- negate -->
+
+        <div class ="seventh_button"> <button type="button" id="button_field"> 9 </button> <!-- 9 -->
 
         </div>
-        <div> <!-- 0 -->
+        <div class ="seventh_button"><button type="button" id="button_field"> x </button> <!-- multiply -->
 
-        </div> <!-- . -->
-        <div> <!-- = -->
+        </div>
+        <div class ="seventh_button"><button type="button" id="button_field"> 4 </button> <!-- 4 -->
+
+        </div>
+        <div class ="seventh_button"><button type="button" id="button_field"> 5 </button> <!-- 5 -->
+
+        </div>
+        <div class ="seventh_button"><button type="button" id="button_field"> 6 </button> <!-- 6 -->
+
+        </div>
+        <div class ="seventh_button"><button type="button" id="button_field"> - </button> <!-- subtract -->
+
+        </div>
+        <div class ="seventh_button"><button type="button" id="button_field"> 1</button> <!-- 1 -->
+
+        </div>
+        <div class ="seventh_button"><button type="button" id="button_field"> 2 </button> <!-- 2 -->
+
+        </div>
+        <div class ="seventh_button"><button type="button" id="button_field"> 3 </button> <!-- 3 -->
+
+        </div>
+        <div class ="seventh_button"><button type="button" id="button_field"> + </button> <!-- addition -->
+
+        </div>
+        <div class ="seventh_button"><button type="button" id="button_field"> +/- </button> <!-- negate -->
+
+        </div>
+        <div class ="seventh_button"><button type="button" id="button_field"> 0 </button> <!-- 0 -->
+
+        </div>
+        <div class ="seventh_button"><button type="button" id="button_field"> , </button> <!-- , -->
+
+        </div>
+        <div class ="seventh_button"><button type="button" id="button_field"> = </button>  <!-- = -->
 
         </div>
     </div>
---
+++ test.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Grid Layout with Top Grid</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="browser">
+    <div class="top-grid">
+        <div class="grid-item"></div>
+        <div class="grid-item"></div>
+        <div class="grid-item"></div>
+    </div>
+    <div class="content">
+        <div class="box red"></div>
+        <div class="box orange"></div>
+        <div class="box yellow"></div>
+        <div class="box green"></div>
+        <div class="box teal"></div>
+        <div class="box turquoise"></div>
+    </div>
+</div>
+</body>
+</html>
